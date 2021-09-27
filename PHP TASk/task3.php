5f9fef25464d583cffa0ff7816ad90f926a124e9
John Doe <johndoe@example.com>
2021-09-27 23:26:11 +0600

    Task3

---
+++ PHP TASk/task3.php
@@ -0,0 +1,12 @@
+<?php  
+$number=10;  
+echo "Given Number is " .$number;
+if($number%2==0)  
+{  
+ echo "<br/>"."$number is Even Number";   
+}  
+else  
+{  
+ echo "<br/>"."$number is Odd Number";  
+}  
+?>
