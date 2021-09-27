8790dca608fe96a2f24f6c760de7598cef61f382
John Doe <johndoe@example.com>
2021-09-27 22:28:25 +0600

    Phptask1

---
+++ PHP TASk/task1.php
@@ -0,0 +1,11 @@
+<?php
+
+   
+   $length=50;
+   $width= 100;
+   $rectangle= $length * $width;
+   $perimeter = 2 * ($length + $width);
+   echo "<br>"."Rectangle: ".$rectangle;
+   echo "<br>"."Perimeter: ".$perimeter;
+
+?>
