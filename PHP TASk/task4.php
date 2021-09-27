186e98b212027842a3a4b3cf36960310355f9d25
John Doe <johndoe@example.com>
2021-09-27 23:47:32 +0600

    Task4

--- PHP TASk/task4.php
+++ PHP TASk/task4.php
@@ -5,7 +5,7 @@
   echo "<br/>"."Second Number is=> ". $number2;
   $number3=30;
   echo "<br/>"."Thrid  Number is=> ". $number3;
-  if($number1>$number2 && $num1>$num3){
+  if($number1>$number2 && $number1>$number3){
         echo "<br/>The Gratter Number is:".$number1;
       }
       elseif($number2>$number1 && $number2>$number3){
