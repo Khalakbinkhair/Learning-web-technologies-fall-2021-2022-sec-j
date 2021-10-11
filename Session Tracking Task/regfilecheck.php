33049b7a9e895526d69d8ba21bfc715a0022834d
John Doe <johndoe@example.com>
2021-10-12 03:12:00 +0600

    pages

--- Session Tracking Task/regfilecheck.php
+++ Session Tracking Task/regfilecheck.php
@@ -4,17 +4,16 @@
 	if(isset($_POST['submit'])){
 
 		if($_POST['username'] !=""){
-				if($_POST['email'] !=""){
-					if($_POST['gender'] !=""){
-						if($_POST['date'] !=""){
-								if($_POST['username'] !=""){
-									  	if($_POST['password'] != ""){
-									  		if($_POST['Confrim password'] != ""){
+			if($_POST['email'] !=""){
+				if($_POST['gender'] !=""){
+				  if($_POST['date'] !=""){
+					if($_POST['username'] !=""){
+						if($_POST['password'] != ""){
+							 if($_POST['checkpassword'] != ""){
+									  	
                      $user = ['username'=> $_POST['username'], 'password'=> $_POST['password']];
 					$_SESSION['user'] = $user;
-					header('location: Loginform.html');
-				}
-			}
+					header('location: Loggedin.html');		
 		}else{
 			echo "Invald username...";
 		}
