a5c1fb94c7dc97fa71ab15f4e99e21cb3156d42a
John Doe <johndoe@example.com>
2021-11-15 16:32:00 +0600

    JS

---
+++ FINAL_JS/Task2/script.js
@@ -0,0 +1,38 @@
+"use strict"
+
+//a =10;
+var b=20;
+let c=30;
+const d=50;
+
+let student = [1, 'alamin', 'alamin@aiub.edu', 2.4];
+
+student[0];
+
+/*
+function abc(){
+	return 0;
+}
+*/
+
+//alert("test");
+//console.log(student[1]);
+//document.write('<h1>this is JS action..</h1>');
+
+function f1(){
+	let element = document.getElementsByTagName('h1')[0];
+	element.innerHTML = "ALAMIN";	
+}
+
+function f2(){
+
+	let name = document.getElementById('name').value;
+	
+	if(name != ""){
+		document.getElementsByTagName('h1')[0].innerHTML = name;
+		//document.getElementById('name').value = "";
+		
+	}
+}
+
+
